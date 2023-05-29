fe518d84666d968fcafb7c4b3c640a21652bc3c4
anouarlamhamdi <anouar.lamhamdi01@gmail.com>
2023-05-29 01:54:47 +0100

    intent

--- app/src/main/java/com/example/mini_projet/MainActivity.java
+++ app/src/main/java/com/example/mini_projet/MainActivity.java
@@ -2,15 +2,18 @@ package com.example.mini_projet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText username;
+    TextView signupText ;
     EditText password;
     Button loginButton;
 
@@ -27,6 +30,14 @@ public class MainActivity extends AppCompatActivity {
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                signupText = (TextView) findViewById(R.id.signupText);
+                signupText.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(MainActivity.this,Sign_up.class);
+                        startActivities(new Intent[]{intent});
+                    }
+                });
                 if (username.getText().toString().equals("user") && password.getText().toString().equals("1234")) {
                     Toast.makeText(MainActivity.this, "Login Successful!", Toast.LENGTH_SHORT).show();
                 } else {
